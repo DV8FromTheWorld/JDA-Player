5beaaf16209fb381bce40bf6af5acab2c6b56ce7
Austin Keener <keeneraustin@yahoo.com>
2016-03-06 05:52:56 +0900

    Abstracted the YT-DL and FFmpeg system into AudioSource and AudioStream.

---
+++ src/main/java/net/dv8tion/jda/player/AudioSource.java
@@ -0,0 +1,134 @@
+package net.dv8tion.jda.player;
+
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Austin on 3/6/2016.
+ */
+public class AudioSource
+{
+    //Defined at the bottom of this file.
+    public static final List<String> YOUTUBE_DL_LAUNCH_ARGS =
+            Collections.unmodifiableList(Arrays.asList(
+                    "python",               //Launch python executor
+                    "./youtube-dl",         //youtube-dl program file
+                    "-q",                   //quiet. No standard out.
+                    "-f", "bestaudio/best", //Format to download. Attempts best audio-only, followed by best video/audio combo
+                    "--no-playlist"         //If the provided link is part of a Playlist, only grabs the video, not playlist too.
+            ));
+    public static final List<String> FFMPEG_LAUNCH_ARGS =
+            Collections.unmodifiableList(Arrays.asList(
+                    "ffmpeg",       //Program launch
+                    "-i", "-",      //Input file, specifies to read from STDin (pipe)
+                    "-f", "mp3"    //Format, type MP3
+            ));
+
+    private final String url;
+    private final List<String> ytdlLaunchArgsF;
+    private final List<String> ffmpegLaunchArgsF;
+
+    public AudioSource(String url)
+    {
+        this(url, null, null);
+    }
+
+    public AudioSource(String url, List<String> ytdlLaunchArgs, List<String> ffmpegLaunchArgs)
+    {
+        if (url == null || url.isEmpty())
+            throw new NullPointerException("String url provided to AudioSource was null or empty.");
+        this.url = url;
+        this.ytdlLaunchArgsF = ytdlLaunchArgs;
+        this.ffmpegLaunchArgsF = ffmpegLaunchArgs;
+    }
+
+    public InputStream asStream()
+    {
+        List<String> ytdlLaunchArgs = new ArrayList<>();
+        List<String> ffmpegLaunchArgs = new ArrayList<>();
+        if (ytdlLaunchArgsF == null)
+            ytdlLaunchArgs.addAll(YOUTUBE_DL_LAUNCH_ARGS);
+        if (ffmpegLaunchArgsF == null)
+            ffmpegLaunchArgs.addAll(FFMPEG_LAUNCH_ARGS);
+
+        ytdlLaunchArgs.add("-o");   //Output location...
+        ytdlLaunchArgs.add("-");    //specifies STDout as the output location (pipe)
+        ytdlLaunchArgs.add(url);    //specifies the URL to download.
+
+        ffmpegLaunchArgs.add("-");  //Used to specify STDout as the output location (pipe)
+
+        return new AudioStream(url,ytdlLaunchArgs, ffmpegLaunchArgs);
+    }
+
+    public File asFile(String path, boolean deleteIfExists) throws FileAlreadyExistsException, FileNotFoundException
+    {
+        if (path == null || path.isEmpty())
+            throw new NullPointerException("Provided path was null or empty!");
+
+        File file = new File(path);
+        if (file.isDirectory())
+            throw new IllegalArgumentException("The provided path is a directory, not a file!");
+        if (file.exists())
+        {
+            if (!deleteIfExists)
+            {
+                throw new FileAlreadyExistsException("The provided path already has an existing file " +
+                        " and the `deleteIfExists` boolean was set to false.");
+            }
+            else
+            {
+                if (!file.delete())
+                    throw new UnsupportedOperationException("Cannot delete the file. Is it in use?");
+            }
+        }
+
+        Thread currentThread = Thread.currentThread();
+        FileOutputStream fos = new FileOutputStream(file);
+        InputStream input = asStream();
+
+        //Writes the bytes of the downloaded audio into the file.
+        //Has detection to detect if the current thread has been interrupted to respect calls to
+        // Thread#interrupt() when an instance of AudioSource is in an async thread.
+        //TODO: consider replacing with a Future.
+        try
+        {
+            byte[] buffer = new byte[1024];
+            int amountRead = -1;
+            int i = 0;
+            while (!currentThread.isInterrupted() && ((amountRead = input.read(buffer)) > -1))
+            {
+                fos.write(buffer, 0, amountRead);
+            }
+            fos.flush();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                input.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            try
+            {
+                fos.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return file;
+    }
+}
---
+++ src/main/java/net/dv8tion/jda/player/AudioStream.java
@@ -0,0 +1,215 @@
+package net.dv8tion.jda.player;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Austin on 3/6/2016.
+ */
+public class AudioStream extends BufferedInputStream
+{
+    //Represent the processes that control the Python Youtube-dl and the FFmpeg program.
+    private Process ytdlProcess;
+    private Process ffmpegProcess;
+
+    //Async threads that deal with the piping of the outputs of the processes.
+    private Thread ytdlToFFmpegThread;
+    private Thread ytdlErrGobler;
+    private Thread ffmpegErrGobler;
+
+    private String url;
+    private List<String> ytdlLaunchArgs;
+    private List<String> ffmpegLaunchArgs;
+
+    protected AudioStream(String url, List<String> ytdlLaunchArgs, List<String> ffmpegLaunchArgs)
+    {
+        super(null);
+        this.url = url;
+        this.ytdlLaunchArgs = ytdlLaunchArgs;
+        this.ffmpegLaunchArgs = ffmpegLaunchArgs;
+        setup();
+    }
+
+    private void setup()
+    {
+        try
+        {
+            ProcessBuilder pBuilder = new ProcessBuilder();
+
+            pBuilder.command(ytdlLaunchArgs);
+            System.out.println("Command: " + pBuilder.command());
+            ytdlProcess = pBuilder.start();
+
+            pBuilder.command(ffmpegLaunchArgs);
+            System.out.println("Command: " + pBuilder.command());
+            ffmpegProcess = pBuilder.start();
+
+            final Process ytdlProcessF = ytdlProcess;
+            final Process ffmpegProcessF = ffmpegProcess;
+
+            ytdlToFFmpegThread = new Thread()
+            {
+                @Override
+                public void run()
+                {
+                    InputStream fromYTDL = null;
+                    OutputStream toFFmpeg = null;
+                    try
+                    {
+                        fromYTDL = ytdlProcessF.getInputStream();
+                        toFFmpeg = ffmpegProcessF.getOutputStream();
+
+                        byte[] buffer = new byte[1024];
+                        int amountRead = -1;
+                        while (!isInterrupted() && ((amountRead = fromYTDL.read(buffer)) > -1))
+                        {
+                            toFFmpeg.write(buffer, 0, amountRead);
+                        }
+                        toFFmpeg.flush();
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    finally
+                    {
+                        try
+                        {
+                            if (fromYTDL != null)
+                                fromYTDL.close();
+                        }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace();
+                        }
+                        try
+                        {
+                            if (toFFmpeg != null)
+                                toFFmpeg.close();
+                        }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            };
+
+            ytdlErrGobler = new Thread()
+            {
+                @Override
+                public void run()
+                {
+
+                    try
+                    {
+                        InputStream fromYTDL = null;
+
+                        fromYTDL = ytdlProcessF.getErrorStream();
+                        if (fromYTDL == null)
+                            System.out.println("fromYTDL is null");
+
+                        byte[] buffer = new byte[1024];
+                        int amountRead = -1;
+                        int i = 0;
+                        while (!isInterrupted() && ((amountRead = fromYTDL.read(buffer)) > -1))
+                        {
+                            i++;
+                            System.out.println("ERR YTDL: " + new String(Arrays.copyOf(buffer, amountRead)));
+                        }
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            ffmpegErrGobler = new Thread()
+            {
+                @Override
+                public void run()
+                {
+                    try
+                    {
+                        InputStream fromFFmpeg = null;
+
+                        fromFFmpeg = ffmpegProcessF.getErrorStream();
+                        if (fromFFmpeg == null)
+                            System.out.println("fromYTDL is null");
+
+                        byte[] buffer = new byte[1024];
+                        int amountRead = -1;
+                        int i = 0;
+                        while (!isInterrupted() && ((amountRead = fromFFmpeg.read(buffer)) > -1))
+                        {
+                            i++;
+                            System.out.println("ERR FFMPEG: " + new String(Arrays.copyOf(buffer, amountRead)));
+                        }
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            ytdlToFFmpegThread.start();
+            ytdlErrGobler.start();
+            ffmpegErrGobler.start();
+            this.in = ffmpegProcess.getInputStream();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            try
+            {
+                close();
+            }
+            catch (IOException e1)
+            {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+        if (in != null)
+        {
+            in.close();
+            in = null;
+        }
+        if (ytdlToFFmpegThread != null)
+        {
+            ytdlToFFmpegThread.interrupt();
+            ytdlToFFmpegThread = null;
+        }
+        if (ytdlErrGobler != null)
+        {
+            ytdlErrGobler.interrupt();
+            ytdlErrGobler = null;
+        }
+        if (ffmpegErrGobler != null)
+        {
+            ffmpegErrGobler.interrupt();
+            ffmpegErrGobler = null;
+        }
+        if (ffmpegProcess != null)
+        {
+            ffmpegProcess.destroy();
+            ffmpegProcess = null;
+        }
+        if (ytdlProcess != null)
+        {
+            ytdlProcess.destroy();
+            ytdlProcess = null;
+        }
+        super.close();
+    }
+}
