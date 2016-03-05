759fb92dd0410e381cb107a385e99a94f69fc683
Austin Keener <keeneraustin@yahoo.com>
2016-03-06 05:53:23 +0900

    Updated the MyPlayer example to use the new system.

--- src/main/java/net/dv8tion/jda/player/MyPlayer.java
+++ src/main/java/net/dv8tion/jda/player/MyPlayer.java
@@ -1,22 +1,17 @@
 package net.dv8tion.jda.player;
 
 import net.dv8tion.jda.audio.player.Player;
-import net.dv8tion.jda.utils.SimpleLog;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.util.Arrays;
 
 /**
  * Created by Austin on 3/4/2016.
  */
 public class MyPlayer extends Player
 {
+    protected String url;
     protected boolean started = false;
     protected boolean playing = false;
     protected boolean paused = false;
@@ -24,168 +19,18 @@ public class MyPlayer extends Player
 
     public MyPlayer(String url)
     {
-
-        Process ytdlProcess = null;
-        Process ffmpegProcess = null;
-        Thread ffmpegToStdout = null;
-        Thread ytdlToFFmpegThread = null;
-
-//        String url = "https://soundcloud.com/ahsoftware/v4-kaai-yuki_demo-song02-full-ver";   //If I had you
-        File ytdlFile = new File("youtube-dl");
-
+        this.url = url;
+        AudioSource source = new AudioSource(url);
         try
         {
-            ProcessBuilder pBuilder = new ProcessBuilder();
-
-            pBuilder.command(
-                    "python",
-                    ytdlFile.getCanonicalPath(),
-                    "-q",   //quiet. No standard out.
-                    "-f", "bestaudio/best",
-                    "--no-playlist",
-//                    "--yes-playlist",
-                    String.format("\"%s\"", url),
-                    "-o", "-"
-            );
-            System.out.println("Command: " + pBuilder.command());
-            ytdlProcess = pBuilder.start();
-
-            pBuilder.command(
-                    "ffmpeg",
-                    "-i", "-",
-                    "-f", "mp3",
-                    "-"
-            );
-            System.out.println("Command: " + pBuilder.command());
-            ffmpegProcess = pBuilder.start();
-
-            final Process ytdlProcessF = ytdlProcess;
-            final Process ffmpegProcessF = ffmpegProcess;
-            ytdlToFFmpegThread = new Thread()
-            {
-                @Override
-                public void run()
-                {
-                    InputStream fromYTDL = null;
-                    OutputStream toFFmpeg = null;
-                    try
-                    {
-                        fromYTDL = ytdlProcessF.getInputStream();
-                        if (fromYTDL == null)
-                            System.out.println("fromYTDL is null");
-
-                        toFFmpeg = ffmpegProcessF.getOutputStream();
-                        if (toFFmpeg == null)
-                            System.out.println("toFFmpeg is null");
-
-                        byte[] buffer = new byte[1024];
-                        int amountRead = -1;
-                        int i = 0;
-                        while (!isInterrupted() && ((amountRead = fromYTDL.read(buffer)) > -1))
-                        {
-                            i++;
-                            System.out.println("Writing: " + amountRead + "  " + i);
-                            toFFmpeg.write(buffer, 0, amountRead);
-                        }
-                        toFFmpeg.flush();
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                    finally
-                    {
-                        System.out.println("The thread has finished?");
-                        try
-                        {
-                            if (fromYTDL != null)
-                                fromYTDL.close();
-
-                            if (toFFmpeg != null)
-                                toFFmpeg.close();
-                        }
-                        catch (IOException e)
-                        {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            };
-
-            Thread t = new Thread()
-            {
-                @Override
-                public void run()
-                {
-
-                    try
-                    {
-                    InputStream fromYTDL = null;
-                    InputStream fromFFmpeg = null;
-
-                        fromYTDL = ytdlProcessF.getErrorStream();
-                        if (fromYTDL == null)
-                            System.out.println("fromYTDL is null");
-
-                        byte[] buffer = new byte[1024];
-                        int amountRead = -1;
-                        int i = 0;
-                        while (!isInterrupted() && ((amountRead = fromYTDL.read(buffer)) > -1))
-                        {
-                            i++;
-                            System.out.println("ERR YTDL: " + new String(Arrays.copyOf(buffer, amountRead)));
-                        }
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-            };
-
-            Thread t2 = new Thread()
-            {
-                @Override
-                public void run()
-                {
-
-                    try
-                    {
-                        InputStream fromFFmpeg = null;
-
-                        fromFFmpeg = ffmpegProcessF.getErrorStream();
-                        if (fromFFmpeg == null)
-                            System.out.println("fromYTDL is null");
-
-                        byte[] buffer = new byte[1024];
-                        int amountRead = -1;
-                        int i = 0;
-                        while (!isInterrupted() && ((amountRead = fromFFmpeg.read(buffer)) > -1))
-                        {
-                            i++;
-                            System.out.println("ERR FFMPEG: " + new String(Arrays.copyOf(buffer, amountRead)));
-                        }
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-            };
-
-            ytdlToFFmpegThread.start();
-            t.start();
-            t2.start();
-            setAudioSource(AudioSystem.getAudioInputStream(ffmpegProcess.getInputStream()));
+            this.setAudioSource(AudioSystem.getAudioInputStream(source.asStream()));
         }
-        catch (IOException | UnsupportedAudioFileException e)
+        catch (UnsupportedAudioFileException e)
         {
-            if (ytdlToFFmpegThread != null)
-                ytdlToFFmpegThread.interrupt();
-            if (ffmpegProcess != null)
-                ffmpegProcess.destroy();
-            if (ytdlProcess != null)
-                ytdlProcess.destroy();
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
@@ -216,39 +61,34 @@ public class MyPlayer extends Player
         playing = false;
         paused = false;
         stopped = true;
-//        try
-//        {
-//            bufferedResourceStream.close();
-//            resourceStream.close();
-//        }
-//        catch (IOException e)
-//        {
-//            SimpleLog.getLog("JDAPlayer").fatal("Attempted to close the URLPlayer resource stream during stop() cleanup, but hit an IOException");
-//            SimpleLog.getLog("JDAPlayer").log(e);
-//        }
+        try
+        {
+            audioSource.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void restart()
     {
-//        URL oldUrl = urlOfResource;
-//        try
-//        {
-//            bufferedResourceStream.close();
-//            resourceStream.close();
-//            reset();
-//            setAudioUrl(oldUrl);
-//            play();
-//        }
-//        catch (IOException e)
-//        {
-//            SimpleLog.getLog("JDAPlayer").fatal("Attempted to restart the URLStream playback, but something went wrong!");
-//            SimpleLog.getLog("JDAPlayer").log(e);
-//        }
-//        catch (UnsupportedAudioFileException e)
-//        {
-//            SimpleLog.getLog("JDAPlayer").log(e);
-//        }
+        try
+        {
+            reset();
+            AudioSource source = new AudioSource(url);
+            this.setAudioSource(AudioSystem.getAudioInputStream(source.asStream()));
+            play();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        catch (UnsupportedAudioFileException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -281,7 +121,14 @@ public class MyPlayer extends Player
         playing = false;
         paused = false;
         stopped = true;
-//        urlOfResource = null;
-//        resourceStream = null;
+        try
+        {
+            audioSource.close();
+            audioSource = null;
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
